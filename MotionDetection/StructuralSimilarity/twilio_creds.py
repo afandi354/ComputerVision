88685db36852a8ac2fd824f90f9c5e341bebcf7c
afandi354 <afandi.webmaster@gmail.com>
2020-02-09 22:16:35 +0700

    Delete Creds

--- MotionDetection/StructuralSimilarity/twilio_creds.py
+++ MotionDetection/StructuralSimilarity/twilio_creds.py
@@ -1,8 +1,3 @@
 ACCOUNT_SID = 'Your_SID'
 AUTH_TOKEN = 'Your Token'
 TWILIO_PHONE = 'Your Phone'
-
-#sid : ACcf1b4e2e362f2d487e03faf5eb9deb5e
-#token : e9763cdd7127e98701fd5c221a896d7d
-#+622180600691
-#+15406965590
